abc2d8339a55ec69475c90ca19fdbeb17761ee69
GRAS Lucas <lucas.gras@hotmail.fr>
2018-05-28 01:45:05 +0200

    [+] Fix warnings

--- PreProcessor/cor/memory.h
+++ PreProcessor/cor/memory.h
@@ -6,14 +6,14 @@
 #define MINILIBC_MEMORY_H
 
 /* ALLOCATION */
-#define MEM_ALLOC(type, size) malloc(sizeof(type) * size)
+#define MEM_ALLOC(type, size) malloc(sizeof(type) * (size))
 #define MEM_PROTECT_ALLOC(ptr, critical) if (!(ptr)) {RETURN(critical);};
 
 #define MEM_FREE(ptr) free(ptr)
 
 /* POINTERS OP */
 
-#define POINT_OFFSET_PTR(ptr, offset, ptrval) *(ptr + offset) = *ptrval;
-#define POINT_OFFSET_VAL(ptr, offset, value) *(ptr + offset) = value;
+#define POINT_OFFSET_PTR(ptr, offset, ptrval) *((ptr) + (offset)) = *(ptrval);
+#define POINT_OFFSET_VAL(ptr, offset, value) *((ptr) + (offset)) = value;
 
 #endif //MINILIBC_MEMORY_H
