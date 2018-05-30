024ad32bf6bf627d2114c36e88328d724febdfd2
GRAS Lucas <lucas.gras@hotmail.fr>
2018-05-30 09:24:01 +0200

    [~] Change python script to POO

---
+++ PreProcessor/tests/class_tester.py
@@ -0,0 +1,34 @@
+# content of class_tester.py
+
+class Tester(object):
+
+    def __init__(self, _stream):
+        self.debug_success = 0
+        self.debug_failure = 1
+        self.debug_status = self.debug_success
+        self.stream = _stream
+
+    def GetListDebug(self):
+        list = ["t\n", "This is a test\n", "strlen=12\n", "strcpy=test strcpy\n",
+                "is_duped\n", "This is a test\n", "a test\n", "0 1\n"]
+        return list
+
+    def GetFuncList(self):
+        list = ["putc", "putstr", "strlen", "strcpy", "strdup", "strcat", "index",
+                "strcmp"]
+        return list
+
+    def SetStream(self, _in):
+        self.stream = _in
+
+    def TestDebug(self, _exp):
+        if (self.stream == _exp):
+            return self.debug_success
+        return self.debug_failure
+
+    def DoTest(self, _exp):
+        if (self.TestDebug(_exp) == self.debug_success):
+            print("\033[32m[OK]\033[m")
+        else:
+            print("\033[31m[KO]\033[m Invalid output: " + _exp)
+            self.debug_status = self.debug_failure
--- PreProcessor/tests/tester_wrapper.py
+++ PreProcessor/tests/tester_wrapper.py
@@ -1,40 +1,24 @@
 # content of test_sample.py
 
 from sys import argv
+from class_tester import *
 
-DEBUG_SUCCESS=(1)
-DEBUG_FAILURE=(-1)
-
-test_debug = lambda stream, expected: DEBUG_SUCCESS if (stream == expected) else DEBUG_FAILURE
-
-def list_debug():
-    list = ["t\n", "This is a test\n", "strlen=12\n", "strcpy=test strcpy\n",
-            "is_duped\n", "This is a test\n", "a test\n", "0 1\n"]
-    return list
-
-def func_list():
-    list = ["putc", "putstr", "strlen", "strcpy", "strdup", "strcat", "index",
-                "strcmp"]
-    return list
-
-def test(stream, expected):
-    if (test_debug(stream, expected) == DEBUG_SUCCESS):
-        print("\033[32m[OK]\033[m")
-    else:
-        print("\033[31m[KO]\033[m expected is " + expected + " but output is " + stream)
+def wrapper(stream, m_Tester):
+    inc = 0
+    list = m_Tester.GetListDebug()
+    flist = m_Tester.GetFuncList()
+    for line in stream:
+        m_Tester.SetStream(line)
+        print("Test running: " + flist[inc] + " ", end="")
+        m_Tester.DoTest(list[inc])
+        inc += 1
 
 def main(argv):
-    inc = 0
     stream = open(argv[1], 'r')
-    stream.readline()
-    list = list_debug()
-    flist = func_list()
-    for line in stream:
-        print("Test running: " + flist[inc] + " ", end="")
-        test(line, list[inc])
-        inc += 1
+    m_Tester = Tester(stream.readline())
+    wrapper(stream, m_Tester)
     stream.close()
-    return 0
+    return m_Tester.debug_status
 
 if (len(argv) == 2):
     main(argv)
