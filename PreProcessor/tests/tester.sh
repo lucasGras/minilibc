ca875a9ccfb6661f87e35f69a67cdb70ead3ba11
GRAS Lucas <lucas.gras@hotmail.fr>
2018-05-31 14:18:27 +0200

    [-] Remove Gem ; [+] bash script

--- PreProcessor/tests/tester.sh
+++ PreProcessor/tests/tester.sh
@@ -3,8 +3,11 @@
 PYWRAPPER="tester_wrapper.py"
 LIB_OUT="tester_out"
 
+echo -ne "[run_tests.sh]Search binary\n"
 cp ../../cmake-build-debug/minilibc .
+echo -ne "[run_tests.sh]Exec binary\n"
 ./minilibc run_tests > $LIB_OUT
-echo -ne "Exec Python\n\n"
+echo -ne "[run_tests.sh]Exec Python\n"
 python3 $PYWRAPPER $LIB_OUT
+echo -ne "[run_tests.sh]Clean tests\n"
 rm -f $LIB_OUT
--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,7 @@ EXIT_FAILURE=84
 
 function checkSuccess()
 {
-    if [ $? -eq 0 ]; then
+    if [ $? -ne 0 ]; then
         echo -ne "\033[31mPrevious command failed\033[m"
         exit ${EXIT_FAILURE}
     fi;
@@ -15,32 +15,33 @@ function checkSuccess()
 
 function _info()
 {
-    echo "-----Infos-----"
-    echo "Language: C ; OS: Linux"
+    echo "[run_tests.sh]-----Infos-----"
+    echo "[run_tests.sh]Language: C ; OS: Linux"
     echo -ne "[run_tests.sh]\033[32mPreparing compilation\033[m\n"
 }
 
 function _compile()
 {
     if [ -d ${BUILD_DIR} ]; then
-        echo "\033[31mBuild directory already exists\033[m"
+        echo -ne "[run_tests.sh]\033[31mBuild directory already exists\033[m\
+                    \n[run_tests.sh]Removing build directory\n"
         rm -rf ${BUILD_DIR}
     fi;
-    mkdir ${BUILD_DIR}
-    cd ${BUILD_DIR}
-    cmake .. && make
-    cd ..
+    mkdir ${BUILD_DIR} && checkSuccess
+    cd ${BUILD_DIR} && checkSuccess
+    cmake .. && make && checkSuccess
+    cd .. && checkSuccess
 }
 
 function _tester_()
 {
-    cd PreProcessor/tests/
+    cd PreProcessor/tests/ && checkSuccess
     if [ -f ${TESTER_SCRIPT} ];
     then
         echo -ne "[run_tests.sh]Run tester.sh\n"
         ./${TESTER_SCRIPT}
     else
-        echo -ne "Can't find tester.sh\n"
+        echo -ne "[run_tests.sh]Can't find tester.sh\n"
         exit ${EXIT_FAILURE}
     fi;
 }
