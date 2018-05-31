b4a501d823d9139f934b8b5ef4ec7ff3183007ec
GRAS Lucas <lucas.gras@hotmail.fr>
2018-05-31 17:09:50 +0200

    [~] Add C demo and add function in bash script

--- PreProcessor/src/demo.c
+++ PreProcessor/src/demo.c
@@ -3,3 +3,17 @@
 //
 
 #include "preproc_minilib.h"
+
+/*
+ * __demo__ function
+ * Usage of preprocessor functions in common C
+ * The main function is located in the preproc_minilib.h file
+ */
+
+void	__demo__(void)
+{
+    int a = MAIN_PREFIXpp_strcmp("This is a test", "This is a");
+
+    printf("strcmp result=%d\n", a);
+    MAIN_PREFIXpp_putstr("demo");
+}
--- PreProcessor/tests/tester.sh
+++ PreProcessor/tests/tester.sh
@@ -2,17 +2,42 @@
 
 PYWRAPPER="tester_wrapper.py"
 LIB_OUT="tester_out"
+EXIT_STATUS=0
 
-if [ -f ${LIB_OUT} ]; then
-    echo -ne "[run_tests.sh]\033[31mtester_out already exists\033[m\
-                \n[run_tests.sh]Removing tester_out file\n"
-    rm -f ${LIB_OUT}
-fi;
+function python_UT()
+{
+    if [ -f ${LIB_OUT} ]; then
+        echo -ne "[run_tests.sh]\033[31mtester_out already exists\033[m\
+                    \n[run_tests.sh]Removing tester_out file\n"
+        rm -f ${LIB_OUT}
+    fi;
 
-echo -ne "[run_tests.sh]Search binary\n"
-cp ../../cmake-build-debug/minilibc .
-echo -ne "[run_tests.sh]Exec binary\n"
-./minilibc run_tests > ${LIB_OUT}
-echo -ne "[run_tests.sh]Exec Python\n"
-python3 ${PYWRAPPER} ${LIB_OUT}
-echo -ne "[run_tests.sh]Clean tests\n"
+    echo -ne "[run_tests.sh]Search binary\n"
+    cp ../../cmake-build-debug/minilibc .
+    echo -ne "[run_tests.sh]Exec binary\n"
+    ./minilibc run_tests > ${LIB_OUT}
+    echo -ne "[run_tests.sh]Exec Python\n"
+    python3 ${PYWRAPPER} ${LIB_OUT}
+    echo -ne "[run_tests.sh]Clean tests\n"
+}
+
+function check_input()
+{
+    echo -ne "Enter:\n-- 0: Exec python unit tests\n-- 1: Quit\n"
+    read input
+
+    if [ ${input} -eq "0" ]; then
+        python_UT
+    elif [ ${input} -eq "1" ]; then
+        exit ${EXIT_STATUS}
+    else
+        check_input
+    fi;
+}
+
+function __tester__()
+{
+    check_input
+}
+
+__tester__
