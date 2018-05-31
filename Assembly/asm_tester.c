10adf7ae11b2b3292d23afdf55098c74c5fcef00
GRAS Lucas <lucas.gras@hotmail.fr>
2018-05-31 17:54:22 +0200

    [-] travis key

--- Assembly/asm_tester.c
+++ Assembly/asm_tester.c
@@ -6,9 +6,9 @@
 #include <stdio.h>
 #include <string.h>
 
+extern int asm_strlen(char *str);
+
 int	main(int ac, char **av)
 {
-    extern int asm_strlen(char *str);
-
     printf("ASM strlen: %d; SYSTEM strlen: %d\n", asm_strlen("Test7654"), (int)strlen("Test7654"));
 }
