33c6e19cf2216032fb17598006d3ac13ccb49754
GRAS Lucas <lucas.gras@hotmail.fr>
2018-06-02 14:00:39 +0200

    [+] Add ASM memcpy and strchr + tester update

--- Assembly/asm_tester.c
+++ Assembly/asm_tester.c
@@ -12,6 +12,8 @@ extern void	asm_write(int fd, char *buff, size_t len);
 extern void	*asm_memset(void *s, int c, size_t n);
 extern int	asm_strcmp(char *s1, char *s2);
 extern int 	asm_strncmp(char *s1, char *s2, size_t n);
+extern char     *asm_strchr(char *s, int c);
+extern void	*asm_memcpy(void *dest, void *src, size_t n);
 
 int	main(int ac, char **av)
 {
@@ -22,14 +24,22 @@ int	main(int ac, char **av)
     printf("strnlen; ASM:%d; SYSTEM:%d\n", asm_strnlen("Test7654", 6), (int)strnlen("Test7654", 6));
 
     /* memset */
-    char	str[20];
-    char	sys[20];
+    char	str_set[20];
+    char	sys_set[20];
 
-    strcpy(str, "This is a test");
-    strcpy(sys, "This is a test");
-    asm_memset(str, '$', 4);
-    memset(sys, '$', 4);
-    printf("memset; ASM:%s; SYSTEM:%s\n", str, sys);
+    strcpy(str_set, "This is a test");
+    strcpy(sys_set, "This is a test");
+    asm_memset(str_set, '$', 4);
+    memset(sys_set, '$', 4);
+    printf("memset; ASM:%s; SYSTEM:%s\n", str_set, sys_set);
+
+    /* memcpy */
+    char	str_cpy[20];
+    char	sys_cpy[20];
+
+    asm_memcpy(str_cpy, "This is a test", 6);
+    memcpy(sys_cpy, "This is  a test", 6);
+    printf("memcpy; ASM:%s; SYSTEM:%s\n", str_cpy, sys_cpy);
 
     /* strcmp */
     char	s1[] = "This";
@@ -41,7 +51,10 @@ int	main(int ac, char **av)
     char	s3[] = "This is awesome";
     char	s4[] = "This is incredible";
 
-    printf("strcmp; ASM:%d; SYSTEM:%d\n", asm_strncmp(s3, s4, 6), strncmp(s3, s4, 6));
+    printf("strncmp; ASM:%d; SYSTEM:%d\n", asm_strncmp(s3, s4, 6), strncmp(s3, s4, 6));
+
+    /* strchr */
+    printf("strchr; ASM:%s; SYSTEM:%s\n", asm_strchr(s3, 'a'), strchr(s3, 'a'));
 
     return 0;
 }
